65b889b5065100cf2d44a820a9017e728f79b752
armin.ytaheri@gmail.com <armin.ytaheri@gmail.com>
2019-04-03 16:01:26 -0400

    [ADD] test script for userfile upload

--- test/file-upload-test.sh
+++ test/file-upload-test.sh
@@ -1,4 +1,6 @@
-curl localhost:4000/ \
-  -F operations='{ "query": "mutation ($input: UserfileInput!) { uploadUserfile(input: $input) { id, name }  }", "variables": { "input": { "uploadFile": null, "dataProviderId": "2", "groupId": "2", "fileType": "text_file", "extract": false }  }  }' \
+echo "Token: $1"
+
+curl -H "Authorization: Bearer $1" localhost:4000/ \
+  -F operations='{ "query": "mutation ($input: UserfileInput!) { uploadUserfile(input: $input) { id, name }  }", "variables": { "input": { "uploadFile": null, "dataProviderId": "2", "groupId": "2", "fileType": "SingleFile", "extract": false }  }  }' \
   -F map='{ "0": ["variables.input.uploadFile"]  }' \
   -F 0=@a.txt

4ec7baa0bd61dfbb2232f3ce8fba3e79ad3d137b
candicecz <czech.candice@gmail.com>
2020-02-07 17:09:49 -0500

    [FIX] parseInt users  issue

--- server/schema/Users/loaders.js
+++ server/schema/Users/loaders.js
@@ -4,7 +4,7 @@ const { relativeURL } = require("./resolvers");
 const batchGetUsers = async (ids, context) => {
   try {
     return ids.map(async id => {
-      return await context.query(`${relativeURL}/${id}`);
+      return await context.query(`${relativeURL}/${+id}`);
     });
   } catch (err) {
     throw err;
